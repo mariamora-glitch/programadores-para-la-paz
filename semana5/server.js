1b577232de297e69cd180ef12e2abf9588128d06
mariamora-glitch <psicomariamora@gmail.com>
2026-07-02 13:36:25 -0500

    Update server.js

--- semana5/server.js
+++ semana5/server.js
@@ -1,3 +1,12 @@
 http://localhost:3000
 http://localhost:3000/saludo
 http://localhost:3000/mensaje/juan 
+app.use(express.json());
+
+app.post('/reporte', (req, res) => {
+  const mensaje = req.body.mensaje;
+  res.json({
+    estado: "Reporte recibido",
+    mensaje: mensaje
+  });
+});
